33b2d8ec8dd3515ffbce28d23a722a1f6ade7a12
Pavel <m83819@gmail.com>
2022-08-24 02:35:16 +0700

    Task002_IsTriangle

---
+++ Task002_IsTriange/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход три 
+числа и проверяет, может ли существовать треугольник с сторонами 
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника 
+меньше суммы двух других сторон
+*/
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+bool IsTriangle(int a, int b, int c)
+{
+    if((a<(b+c))&&(b<(a+c))&&(c<(a+b))) return true;
+    return false;
+}
+
+int a=Prompt("enter a side:  ");
+int b=Prompt("enter b side:  ");
+int c=Prompt("enter c side:  ");
+
+if(IsTriangle(a,b,c)) {Console.WriteLine($"the triangle with sides a,b,c may exist");}
+else {Console.WriteLine($"the triangle with sides a,b,c may not exist");}
+
